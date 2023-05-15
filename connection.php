674bb454c00ea1ceb505809c80ac641d0c30f48f
scopyy8 <133508998+scopyy8@users.noreply.github.com>
2023-05-15 13:19:13 -0700

    ..

---
+++ connection.php
@@ -0,0 +1,11 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "school";
+
+$db = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+$sql = "SET NAMES 'utf8'";
+$result = $db->prepare($sql);
+$result->execute();
