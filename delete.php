ba5fde3be3626541ac428e3802eb910285b7960a
scopyy8 <133508998+scopyy8@users.noreply.github.com>
2023-05-15 13:21:20 -0700

    .

--- delete.php
+++ delete.php
@@ -10,11 +10,3 @@ if (isset($_GET['id']) && isset($_GET['page'])) {
             $del->execute();
             header("location:index.php");
             break;
-        case 2:
-            $query = "DELETE FROM student WHERE stud_id=" . $id;
-            $del = $db->prepare($query);
-            $del->execute();
-            header("location:index.php");
-            break;
-    }
-}
